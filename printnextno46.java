30b3a788042603558ae8512bf3051c0a2e9cdec5
likki148 <41731281+likki148@users.noreply.github.com>
2018-08-29 11:59:19 +0530

    Create printnextno46.java

---
+++ printnextno46.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int s;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextInt();
+		s=s+1;
+		System.out.print(s);
+ 
+	}
+}
